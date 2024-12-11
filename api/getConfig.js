c4a7a56797df7cd44447433e8bce63a3a17dabed
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2024-12-11 17:08:44 +0100

    Update getConfig.js

--- api/getConfig.js
+++ api/getConfig.js
@@ -1,5 +1,7 @@
 module.exports = (req, res) => {
     res.json({
-        clientId: process.env.GC_OAUTH_CLIENT_ID
+        clientId: process.env.GC_OAUTH_CLIENT_ID,
+        awsApiEndpoint: process.env.AWS_API_ENDPOINT,
+        awsApiKey: process.env.AWS_API_KEY
     });
 };
