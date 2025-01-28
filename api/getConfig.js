5983efd5b2d0828059103563c2c30326a44017eb
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2025-01-28 14:01:37 +0100

    Update getConfig.js

--- api/getConfig.js
+++ api/getConfig.js
@@ -1,6 +1,5 @@
 module.exports = (req, res) => {
     res.json({
-        clientId: process.env.GC_OAUTH_CLIENT_ID,
-        redirectUri: process.env.REDIRECT_URI
+        clientId: process.env.GC_OAUTH_CLIENT_ID
     });
 };
